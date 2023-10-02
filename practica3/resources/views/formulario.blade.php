d680b62b7c08478860f245a43691f8143ba502da
monserratvargass <guvamonse@gmail.com>
2023-10-02 11:51:18 -0600

    Rutas, vistas, blade

---
+++ practica3/resources/views/formulario.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.plantilla') 
+
+@section('titulo','Formulario')
+
+@section('contenido')
+
+<h1 class="display-1 text-center text-danger mt-5">Formulario</h1>
+
+@endsection
---
+++ practica3/resources/views/recuerdos.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.plantilla') 
+
+@section('titulo','Recuerdos')
+
+@section('contenido')
+
+<h1 class="display-1 text-center text-danger mt-5">Recuerdos</h1>
+
+@include('partials.pagination')
+
+@endsection
