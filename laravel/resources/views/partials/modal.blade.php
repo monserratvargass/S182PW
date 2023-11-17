bbd321688099c201f7ccd0fee899db9c5e770666
monserratvargass <guvamonse@gmail.com>
2023-11-17 16:48:17 -0600

    update button

---
+++ laravel/resources/views/partials/modal.blade.php
@@ -0,0 +1,32 @@
+    <!-- Modal -->
+  <div class="modal fade" id="editar" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>{{-- cierra body --}}
+        <div class="modal-body">
+            <form method="POST" action="#">
+                @csrf 
+                  <div class="mb-3">
+                    <label class="form-label">Titulo:</label>
+                    <input type="text" name="txtTitulo" class="form-control" value="{{old('txtTitulo')}}">
+                    <p class="text-danger"> {{$errors->first('txtTitulo')}} </p> <!-- invocar la variable echo -->
+                  </div>
+      
+                  <div class="mb-3">
+                    <label class="form-label">Recuerdo:</label>
+                    <input type="text" name="txtRecuerdo" class="form-control" value="{{old('txtRecuerdo')}}">
+                    <p class="text-danger"> {{$errors->first('txtRecuerdo')}} </p>
+                  </div>      
+        </div>
+        <div class="modal-footer">
+            <div class="card-footer text-muted">
+                <button type="submit" class="btn btn-primary">Editar recuerdo</button>
+            </form>
+            </div>
+        </div>{{-- cierra footer --}}
+      </div>
+    </div>
+  </div>
--- laravel/resources/views/recuerdos.blade.php
+++ laravel/resources/views/recuerdos.blade.php
@@ -8,6 +8,8 @@
 
 @include('partials.pagination')
 
+@include('partials.modal')
+
 @foreach ($consulRecuerdos as $recuerdo)
 
 <div class="card w-75 mb-3 mt-5">
@@ -15,7 +17,10 @@
       <h5 class="card-title fw-semibold">{{ $recuerdo->titulo }}</h5>
       <p class="card-text fst-italic">{{ $recuerdo->fecha }}</p>
       <p class="card-text fst-italic">{{ $recuerdo->recuerdo }}</p>
-      <a href="#" class="btn btn-warning">Editar</a>
+      <!-- Button trigger modal -->
+<button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#editar">
+  Editar
+</button>
       <a href="#" class="card-link">Borrar</a>
     </div>
   </div>
